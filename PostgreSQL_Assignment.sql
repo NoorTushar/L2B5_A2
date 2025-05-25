f1da8724ee997d8e40aa9f596e55d40eea611602
NoorTushar <tushar.noor.khan@gmail.com>
2025-05-25 21:30:52 +0600

    update

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -53,47 +53,47 @@ INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VA
     (1,2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL)
 
 
--- 1. Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
+-- Problem 1. Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
 
 INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains')
 
 
--- 2️. Count unique species ever sighted
+-- Problem 2️. Count unique species ever sighted
 
 SELECT count(DISTINCT species_id) as "unique_species_count" FROM sightings
 
 
--- 3. Find all sightings where the location includes "Pass"
+-- Problem 3. Find all sightings where the location includes "Pass"
 SELECT * FROM sightings WHERE location ILIKE('%Pass%')
 
 
--- 4. List each ranger's name and their total number of sightings.
+-- Problem 4. List each ranger's name and their total number of sightings.
 
 SELECT name, count(sighting_id) as "total_sightings" FROM rangers
     JOIN sightings ON "sightings".ranger_id = "rangers".ranger_id
     GROUP BY name;
 
 
--- 5. List species that have never been sighted.
+-- Problem 5. List species that have never been sighted.
 
 SELECT common_name FROM species
     LEFT JOIN sightings on "sightings".species_id = "species".species_id
     WHERE sighting_id IS NULL;
 
 
--- 6. Show the most recent 2 sightings
+-- Problem 6. Show the most recent 2 sightings
 SELECT  common_name, sighting_time, name from sightings
     JOIN species ON "sightings".species_id = "species".species_id
     JOIN rangers ON "sightings".ranger_id = "rangers".ranger_id
     ORDER BY sighting_time DESC LIMIT 2
 
 
--- 7. Update all species discovered before year 1800 to have status 'Historic'
+-- Problem 7. Update all species discovered before year 1800 to have status 'Historic'
 
 UPDATE species
     SET "conservation_status" =  'Historic' WHERE "discovery_date" < '1800-01-01'
 
--- 8. Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'.
+-- Problem 8. Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'.
 
 -- Morning: before 12 PM
 -- Afternoon: 12 PM–5 PM
@@ -109,7 +109,7 @@ SELECT
 FROM sightings;
 
 
--- 9️. Delete rangers who have never sighted any species
+-- Problem 9️. Delete rangers who have never sighted any species
 
 DELETE FROM rangers WHERE ranger_id IN (
     SELECT "rangers".ranger_id FROM "rangers"
---
+++ README.md
@@ -0,0 +1,9 @@
+## 3. Explain the Primary Key and Foreign Key concepts in PostgreSQL.
+
+## 4. What is the difference between the VARCHAR and CHAR data types?
+
+## 6. What are the LIMIT and OFFSET clauses used for?
+
+## 8. What is the significance of the JOIN operation, and how does it work in PostgreSQL?
+
+## 9. Explain the GROUP BY clause and its role in aggregation operations.
